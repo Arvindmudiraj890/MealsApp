3e643975f95720edb7b2080c8c1481292a24c70d
Arvindmudiraj890 <93713606+Arvindmudiraj890@users.noreply.github.com>
2023-05-18 12:31:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 const meals = [
     { name: "Spaghetti Bolognese", type: "Italian", ingredients: ["spaghetti", "tomato sauce", "ground beef", "onion", "garlic"] },
     { name: "Chicken Tikka Masala", type: "Indian", ingredients: ["chicken", "yogurt", "tomato sauce", "spices"] },
-    { name: "Caesar Salad", type: "American", ingredients: ["lettuce", "croutons", "parmesan cheese", "Caesar dressing"] },
-    { name: "Sushi", type: "Japanese", ingredients: ["rice", "nori", "fish", "vegetables"] },
-    { name: "Taco", type: "Mexican", ingredients: ["tortilla", "ground beef", "lettuce", "tomato", "cheese"] }
+    { name: "Caesar Salad", type: "American", ingredients: ["lettuce", "croutons", "parmesan cheese", "caesar dressing"] },
+    // Add more sample meals here
 ];
 
 // Function to display meals
@@ -12,33 +11,29 @@ function displayMeals() {
     const mealsList = document.getElementById("mealsList");
     mealsList.innerHTML = "";
 
-    for (let i = 0; i < meals.length; i++) {
-        const meal = meals[i];
-
+    meals.forEach(meal => {
         const mealCard = document.createElement("div");
-        mealCard.className = "mealCard";
+        mealCard.classList.add("mealCard");
 
-        const mealName = document.createElement("h2");
-        mealName.textContent = meal.name;
+        const mealTitle = document.createElement("h2");
+        mealTitle.textContent = meal.name;
 
         const mealType = document.createElement("p");
-        mealType.textContent = meal.type;
+        mealType.textContent = `Type: ${meal.type}`;
 
         const mealIngredients = document.createElement("p");
-        mealIngredients.textContent = "Ingredients: " + meal.ingredients.join(", ");
+        mealIngredients.textContent = `Ingredients: ${meal.ingredients.join(", ")}`;
 
-        mealCard.appendChild(mealName);
+        mealCard.appendChild(mealTitle);
         mealCard.appendChild(mealType);
         mealCard.appendChild(mealIngredients);
-
         mealsList.appendChild(mealCard);
-    }
+    });
 }
 
 // Function to filter meals based on search input
 function filterMeals() {
-    const searchInput = document.getElementById("searchInput");
-    const searchTerm = searchInput.value.toLowerCase();
+    const searchTerm = document.getElementById("searchInput").value.toLowerCase();
 
     const filteredMeals = meals.filter(meal => meal.name.toLowerCase().includes(searchTerm) || meal.type.toLowerCase().includes(searchTerm));
 
@@ -57,5 +52,6 @@ searchInput.addEventListener("input", filterMeals);
 
 // Display initial list of meals
 displayMeals();
+
 
 
