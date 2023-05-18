6e0e492b336198274b2af1116f8387ffa6da4734
Arvindmudiraj890 <93713606+Arvindmudiraj890@users.noreply.github.com>
2023-05-18 12:38:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Sample meal data
-const meals = [
+let meals = [
     { name: "Spaghetti Bolognese", type: "Italian", ingredients: ["spaghetti", "tomato sauce", "ground beef", "onion", "garlic"] },
     { name: "Chicken Tikka Masala", type: "Indian", ingredients: ["chicken", "yogurt", "tomato sauce", "spices"] },
     { name: "Caesar Salad", type: "American", ingredients: ["lettuce", "croutons", "parmesan cheese", "caesar dressing"] },
@@ -31,6 +31,29 @@ function displayMeals() {
     });
 }
 
+// Function to add a new meal
+function addMeal() {
+    const nameInput = document.getElementById("nameInput").value;
+    const typeInput = document.getElementById("typeInput").value;
+    const ingredientsInput = document.getElementById("ingredientsInput").value;
+
+    const ingredientsArray = ingredientsInput.split(",").map(ingredient => ingredient.trim());
+
+    const newMeal = {
+        name: nameInput,
+        type: typeInput,
+        ingredients: ingredientsArray
+    };
+
+    meals.push(newMeal);
+    displayMeals();
+
+    // Clear the input fields after adding a meal
+    document.getElementById("nameInput").value = "";
+    document.getElementById("typeInput").value = "";
+    document.getElementById("ingredientsInput").value = "";
+}
+
 // Function to filter meals based on search input
 function filterMeals() {
     const searchTerm = document.getElementById("searchInput").value.toLowerCase();
@@ -38,18 +61,21 @@ function filterMeals() {
     const filteredMeals = meals.filter(meal => meal.name.toLowerCase().includes(searchTerm) || meal.type.toLowerCase().includes(searchTerm));
 
     if (filteredMeals.length > 0) {
-        meals = filteredMeals;
+        displayMeals(filteredMeals);
     } else {
-        meals = [];
+        const mealsList = document.getElementById("mealsList");
+        mealsList.innerHTML = "<p>No meals found.</p>";
     }
-
-    displayMeals();
 }
 
 // Event listener for search input
 const searchInput = document.getElementById("searchInput");
 searchInput.addEventListener("input", filterMeals);
 
+// Event listener for search button
+const searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", filterMeals);
+    
 // Display initial list of meals
 displayMeals();
 
